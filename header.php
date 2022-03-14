30d04e08aec5f66a389924402f038ba0c2d010e8
Henrique Barbosa <henrikleod@gmail.com>
2022-03-14 16:48:38 -0300

    Removing all meta tags (yoast business)

--- header.php
+++ header.php
@@ -6,9 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <?php wp_head(); ?>
